89effd279c3c9ce3e473f9e213bc3d83abd55c96
khrist14n <khrist14n@gmail.com>
2017-10-31 14:15:43 -0500

    upload files ... ajax method simple ...

---
+++ git.sh
@@ -0,0 +1,10 @@
+git add project/app/fonts/. -f
+git add project/app/images/. -f
+git add project/app/php/. -f
+git add project/app/scripts/. -f
+git add project/app/styles/. -f
+git add project/app/index.html -f
+git add project/app/upload.html -f
+git commit -am "upload files ... ajax method simple ..."
+git push origin master
+echo "uploaded files ... "
